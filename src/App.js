dcf592c1016e47b0342d765de578efc1eb558421
ramesh <rameshkumar.v@celestialsys.com>
2025-09-30 19:14:53 +0530

    Added browser roting concepts in the pages

--- src/App.js
+++ src/App.js
@@ -9,36 +9,26 @@ import SearchItem from './SearchItem';
 import './App.css';
 
 import Result from './components/Result';
-const secretNum = Math.floor(Math.random() * 10) + 1;
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 function App() {
-const [term, setTerm] = useState()
-
-
-const handleChange = (e) => {
-  
-   setTerm(e.target.value)
-  //  console.log("setvalue", term)
-}
-
-return (
-  <div className='container'>
-    <div className='head'>
-      <label htmlFor="term">Guess the number between 1 to 10 answer {secretNum}</label>
-    </div>
-
-    <input  
-      id= "term"
-      type='text'
-      name='term'
-      onChange={handleChange}
-    />
+  return(
+    <>
+   <BrowserRouter>
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/contact' element={<Contact/>} />
+    </Routes>
+   </BrowserRouter>
+    </>
+    
+  )
   
 
-    <Result secretNum={secretNum} term={term} />
-
-  </div>
-);
 }
 
 export default App;
